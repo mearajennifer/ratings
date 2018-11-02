d35114d8659fab6ea750bc7d22879e2045db8153
Jennifer Meara <jenniferlmeara@gmail.com>
2018-11-02 01:09:59 +0000

    Add user prediction and judgemental eye ratings for user

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 """Movie Ratings."""
 
 from jinja2 import StrictUndefined
-
 from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.orm.exc import NoResultFound
-
 from model import User, Rating, Movie, connect_to_db, db
 from pprint import pprint
 
@@ -20,11 +18,84 @@ app.secret_key = "ABC"
 # error.
 app.jinja_env.undefined = StrictUndefined
 
+
 # this is the route to the homepage
 @app.route('/')
 def index():
     """Homepage."""
+
     return render_template("homepage.html")
+
+
+@app.route("/register", methods=["GET"])
+def register_form():
+    """Display registration form."""
+
+    return render_template("register.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    """Process user registration and send to homepage."""
+
+    # Get form variables
+    email = request.form['email']
+    password = request.form['password']
+    age = int(request.form['age'])
+    zipcode = request.form['zipcode']
+
+    # check to see if user is in database,
+    # if not, add new user
+    # otherwise tell them a user already exists
+    try:
+        verify_email = User.query.filter(User.email == email).one()
+        if verify_email:
+            flash('A user with that email already exists!')
+            return redirect("/register")
+    except NoResultFound:
+        new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f"User {email} added.")
+        return redirect("/")
+
+
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Shows user login form"""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def verify_login():
+    """Verifies user email is in database and password matches"""
+
+    # gets email and password from form and verifies user in db
+    email = request.form['email']
+    password = request.form['password']
+    user = User.query.filter(User.email == email).first()
+
+    # if they are in db, add session to login, else flash message
+    if user and (user.password == password):
+        session['user_id'] = user.user_id
+        flash('You are logged in... Prepare to be judged!')
+        return redirect("/user_info?user_id=" + str(user.user_id))
+    else:
+        flash('Invalid email or password.')
+        return redirect("/login")
+
+
+@app.route("/logout")
+def logout():
+    """logs the current user out"""
+
+    # removes session from browser to log out
+    del session['user_id']
+
+    flash("You are no longer being judged...")
+    return redirect("/")
+
 
 # queries the users from the User object and lists all users
 @app.route("/users")
@@ -37,34 +108,14 @@ def user_list():
 
 
 # user info page gets user id from linked page
-@app.route("/user_info", methods=["GET"])
-def show_user():
+@app.route("/users/<int:user_id>")
+def show_user(user_id):
     """shows individual user info"""
 
-    # getting the user from the url via jinja
-    user_id = request.args.get('user_id')
-
     # queries the user objects to find the user with matching id and grabs attributes
-    user = User.query.filter(User.user_id == user_id).one()
-    age = user.age
-    zipcode = user.zipcode
+    user = User.query.options(db.joinedload('ratings').joinedload('movie')).get(user_id)
 
-    # queries the rating objects to find current user and lists users ratings
-    user_ratings = Rating.query.filter(Rating.user_id == user.user_id).all()
-    # movie_score = []
-    # for rating in user_ratings:
-    #     movie_score.append((rating.movie.title, rating.score))
-
-    return render_template("user_info.html",
-                           user_id=user_id,
-                           age=age,
-                           zipcode=zipcode,
-                           user_ratings=user_ratings
-                           )
-
-# code added by ashley to show other possibilites
-# @app.route("/user/<user_id>")
-# def show_user(user_id):
+    return render_template("user_info.html", user=user)
 
 
 # lists the movies by query
@@ -79,27 +130,91 @@ def movie_list():
 
 
 # displays individual movie information based on movie_id
-@app.route("/movies/<movie_id>")
+@app.route("/movies/<int:movie_id>", methods=["GET"])
 def show_movie(movie_id):
+    """Show info about movie.
 
-    # queries the movies to find current movie by id
-    movie = Movie.query.filter(Movie.movie_id == movie_id).one()
-    # queries ratings to get all the ratings for current movie by movie_id
-    ratings = Rating.query.filter(Rating.movie_id == movie_id).all()
+    If a user is logged in, let them add/edit a rating.
+    """
 
-    # checks if a user is logged in and displays page accordingly 
-    try:
-        user = User.query.filter(User.email == session['email']).one()
-        user_rating = [rating.score for rating in ratings if rating.user_id == user.user_id]
-        return render_template("movie_details.html",
-                               movie=movie,
-                               ratings=ratings,
-                               user=user,
-                               user_rating=user_rating)
-    except KeyError:
-        return render_template("movie_details.html",
-                               movie=movie,
-                               ratings=ratings)
+    movie = Movie.query.get(movie_id)
+    user_id = session.get('user_id')
+
+    if user_id:
+        user_rating = Rating.query.filter_by(
+            movie_id=movie_id, user_id=user_id).first()
+    else:
+        user_rating = None
+
+    # Get average rating of movie
+    rating_scores = [r.score for r in movie.ratings]
+    avg_rating = float(sum(rating_scores)) / len(rating_scores)
+
+    prediction = None
+
+    # Prediction code: only predict if the user hasn't rated it.
+    if (not user_rating) and user_id:
+        user = User.query.get(user_id)
+        if user:
+            prediction = user.predict_rating(movie)
+
+    # Either use the prediction or their real rating
+    if prediction:
+        # User hasn't scored; use our prediction if we made one
+        effective_rating = prediction
+
+    elif user_rating:
+        # User has already scored for real; use that
+        effective_rating = user_rating.score
+
+    else:
+        # User hasn't scored, and we couldn't get a prediction
+        effective_rating = None
+
+    # Get the eye's rating, either by predicting or using real rating
+    the_eye = User.query.filter_by(email="the-eye@of-judgment.com").one()
+    eye_rating = Rating.query.filter_by(
+        user_id=the_eye.user_id, movie_id=movie.movie_id).first()
+
+    if eye_rating is None:
+        eye_rating = the_eye.predict_rating(movie)
+
+    else:
+        eye_rating = eye_rating.score
+
+    if eye_rating and effective_rating:
+        difference = abs(eye_rating - effective_rating)
+
+    else:
+        # We couldn't get an eye rating, so we'll skip difference
+        difference = None
+
+    # Depending on how different we are from the Eye, choose a message
+    BERATEMENT_MESSAGES = [
+        "I suppose you don't have such bad taste after all.",
+        "I regret every decision that I've ever made that has brought me" +
+            " to listen to your opinion.",
+        "Words fail me, as your taste in movies has clearly failed you.",
+        "Did you watch this movie in an alternate universe where your taste doesn't suck?",
+        "Words cannot express the awfulness of your taste."
+    ]
+
+    if difference:
+        beratement = BERATEMENT_MESSAGES[int(difference)]
+
+    else:
+        beratement = None
+
+    return render_template(
+        "movie_details.html",
+        movie=movie,
+        user_rating=user_rating,
+        average=avg_rating,
+        prediction=prediction,
+        eye_rating=eye_rating,
+        difference=difference,
+        beratement=beratement
+        )
 
 
 @app.route("/movies/<movie_id>", methods=["POST"])
@@ -126,71 +241,6 @@ def rate_movie(movie_id):
 
     flash('You have judged this movie.')
     return redirect("/user_info?user_id=" + str(user.user_id))
-
-
-@app.route("/register", methods=["GET"])
-def register_form():
-    """display register form"""
-
-    return render_template("register.html")
-
-
-@app.route("/register", methods=["POST"])
-def register_process():
-    """process registration and sends you home."""
-
-    # get email and password from form
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    # check to see if user is in database, if not, add new user
-    # otherwise tell them a user already exists
-    try:
-        verify_email = User.query.filter(User.email == email).one()
-        user = User(email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return redirect("/")
-    except NoResultFound:
-        flash('A user with that email already exists!')
-        return redirect("/register")
-
-
-@app.route("/login", methods=["GET"])
-def show_login():
-    """Shows user login form"""
-
-    return render_template("login.html")
-
-
-@app.route("/login", methods=["POST"])
-def verify_login():
-    """Verifies user email is in database and password matches"""
-
-    # gets email and password from form and verifies user in db
-    email = request.form.get('email')
-    password = request.form.get('password')
-    verify_email = User.query.filter(User.email == email).all()
-
-    # if they are in db, add session to login, else flash message
-    if verify_email and (verify_email[0].password == password):
-        session['email'] = email
-        flash('You are logged in, prepare to be judged.')
-        return redirect("/user_info?user_id=" + str(verify_email[0].user_id))
-    else:
-        flash('That login is invalid')
-        return redirect("/login")
-
-
-@app.route("/logout")
-def logout():
-    """logs the current user out"""
-
-    # removes session from browser to log out
-    del session['email']
-
-    flash("you are no longer being judged... on this website.")
-    return redirect("/")
 
 
 if __name__ == "__main__":
